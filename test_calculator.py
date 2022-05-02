427d1048c81cbbe904619d0224fb1d183d6b5f60
Satya150180 <104626418+Satya150180@users.noreply.github.com>
2022-05-02 19:06:01 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -12,9 +12,7 @@ class TestCalculator:
 
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-        
-    def test_multiplication(self):
-        assert 2 == calculator.multiplication(4, 2)
+ 
     
     
         
